d6df19df644905ff88f4b5b899e5403296894102
sukreet <sukreet001@gmail.com>
2016-07-12 18:18:12 +0530

    Dubey, Sukreeto | BDSHR-1323 | Setting up repo for MCI using java 8.

---
+++ mci-api/src/main/java/org/sharedhealth/mci/web/launch/Main.java
@@ -0,0 +1,12 @@
+package org.sharedhealth.mci.web.launch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MCI-Java 8");
+        System.out.println("Press any key to exit");
+        long i = 0;
+        while (i < 100000) {
+            i--;
+        }
+    }
+}
---
+++ mci-api/src/test/java/org/sharedhealth/mci/web/launch/SampleTest.java
@@ -0,0 +1,14 @@
+package org.sharedhealth.mci.web.launch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void shouldPass() throws Exception {
+        assertTrue(true);
+    }
+
+
+}
