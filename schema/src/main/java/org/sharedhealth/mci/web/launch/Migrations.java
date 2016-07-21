044f4640c9c4cc6ffd2098a48f4c2f17c1de46b2
MD Dubey <mdinfotech007@gmail.com>
2016-07-21 13:18:35 +0530

    Dubey, Sukreet | BDSHR-1327 | Added schema module for migrations

---
+++ schema/src/main/java/org/sharedhealth/mci/web/launch/Migrations.java
@@ -0,0 +1,88 @@
+package org.sharedhealth.mci.web.launch;
+
+
+import com.datastax.driver.core.*;
+import com.datastax.driver.core.policies.ConstantReconnectionPolicy;
+import com.datastax.driver.core.policies.RoundRobinPolicy;
+import com.toddfast.mutagen.cassandra.CassandraMutagen;
+import com.toddfast.mutagen.cassandra.CassandraSubject;
+import com.toddfast.mutagen.cassandra.impl.CassandraMutagenImpl;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static java.lang.System.getenv;
+
+public class Migrations {
+
+    public static final int ONE_MINUTE = 6000;
+
+    protected final Map<String, String> env;
+
+    public Migrations() {
+        this(getenv());
+    }
+
+    public Migrations(Map<String, String> env) {
+        this.env = env;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Migrations().migrate();
+    }
+
+    public void migrate() throws IOException {
+        String mciKeyspace = env.get("CASSANDRA_KEYSPACE");
+        Cluster cluster = connectKeyspace();
+        Session session = createSession(cluster);
+        CassandraMutagen mutagen = new CassandraMutagenImpl(mciKeyspace);
+
+        try {
+            mutagen.initialize(env.get("CASSANDRA_MIGRATIONS_PATH"));
+            com.toddfast.mutagen.Plan.Result<Integer> result = mutagen.mutate(new CassandraSubject(session,
+                    mciKeyspace));
+
+            if (result.getException() != null) {
+                throw new RuntimeException(result.getException());
+            } else if (!result.isMutationComplete()) {
+                throw new RuntimeException("Failed to apply cassandra migrations");
+            }
+        } finally {
+            closeConnection(cluster, session);
+        }
+    }
+
+    private void closeConnection(Cluster cluster, Session session) {
+        session.close();
+        cluster.close();
+    }
+
+    private Cluster connectKeyspace() {
+        return connectCluster();
+    }
+
+    protected Cluster connectCluster() {
+        Cluster.Builder clusterBuilder = new Cluster.Builder();
+
+        QueryOptions queryOptions = new QueryOptions();
+        queryOptions.setConsistencyLevel(ConsistencyLevel.QUORUM);
+
+        clusterBuilder
+                .withPort(Integer.parseInt(env.get("CASSANDRA_PORT")))
+                .withClusterName(env.get("CASSANDRA_KEYSPACE"))
+                .withLoadBalancingPolicy(new RoundRobinPolicy())
+                .withPoolingOptions(new PoolingOptions())
+                .withAuthProvider(new PlainTextAuthProvider(env.get("CASSANDRA_USER"), env.get("CASSANDRA_PASSWORD")))
+                .withProtocolVersion(ProtocolVersion.fromInt(Integer.parseInt(env.get("CASSANDRA_VERSION"))))
+                .withQueryOptions(queryOptions)
+                .withReconnectionPolicy(new ConstantReconnectionPolicy(ONE_MINUTE))
+                .addContactPoint(env.get("CASSANDRA_HOST"));
+        return clusterBuilder.build();
+
+    }
+
+    protected Session createSession(Cluster cluster) {
+        String keyspace = env.get("CASSANDRA_KEYSPACE");
+        return cluster.connect(keyspace);
+    }
+}
