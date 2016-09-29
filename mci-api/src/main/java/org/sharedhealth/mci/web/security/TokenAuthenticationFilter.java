f5f6b073e765b69ed2046803c959114391123ec9
anjalyj <anjalyj@thoughtworks.com>
2016-09-29 16:03:12 +0530

    Anjaly,Neha | SHR-1356 | Enabling ehcache.

--- mci-api/src/main/java/org/sharedhealth/mci/web/security/TokenAuthenticationFilter.java
+++ mci-api/src/main/java/org/sharedhealth/mci/web/security/TokenAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package org.sharedhealth.mci.web.security;
 
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.sharedhealth.mci.web.config.MCIProperties;
@@ -27,7 +30,20 @@ public class TokenAuthenticationFilter implements Filter {
     public void handle(Request request, Response response) throws Exception {
         String authToken = request.headers(AUTH_TOKEN_KEY);
 
-        UserInfo userInfo = identityProviderService.getUserInfo(mciProperties, authToken);
+        CacheManager cm = CacheManager.newInstance();
+        Cache cache = cm.getCache("cache1");
+
+        Element element = cache.get(authToken);
+        UserInfo userInfo;
+
+        if(element==null){
+            userInfo = identityProviderService.getUserInfo(mciProperties, authToken);
+            cache.put(new Element(authToken,userInfo));
+        }else{
+            userInfo = (UserInfo) element.getObjectValue();
+        }
+
+
         if (!request.headers(FROM_KEY).equals(userInfo.getProperties().getEmail())
                 || !request.headers(CLIENT_ID_KEY).equals(userInfo.getProperties().getId())) {
             logger.error("Unable to authenticate user");
